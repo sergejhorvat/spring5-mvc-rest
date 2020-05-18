f584fc916b828f17b7afee716d13f67fa759e81b
Sergej Horvat <sergejhorvat@users.noreply.github.com>
2020-05-18 10:13:24 +0200

    Create Bootstrap.java

---
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,48 @@
+package guru.springfamework.bootstrap;
+
+import guru.springfamework.domain.Category;
+import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+// Spring boot events - CommandLineRunner
+public class Bootstrap implements CommandLineRunner {
+
+    public CategoryRepository categoryRepository;
+
+    public Bootstrap(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    // Implement CommandLineRunner
+    @Override
+    public void run(String... args) throws Exception {
+
+        Category fruits = new Category();
+        fruits.setName("Fruits");
+
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        dried.setName("Fresh");
+
+        Category exotic = new Category();
+        dried.setName("Exotic");
+
+        Category nuts = new Category();
+        dried.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+    }
+
+
+
+
+}
