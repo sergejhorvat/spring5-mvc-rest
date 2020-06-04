1b70113a96c13cdbe2a7e2d17cd19911dbbb54ca
Sergej Horvat <sergejhorvat@users.noreply.github.com>
2020-06-04 23:48:03 +0200

    Refactor @Controller to @RestController with @Response type and return ResponseBody insted of ResponseEntity. This is preferred way in Spring 5.

--- src/main/java/guru/springfamework/controllers/v1/CategoryController.java
+++ src/main/java/guru/springfamework/controllers/v1/CategoryController.java
@@ -6,14 +6,13 @@ import guru.springfamework.services.CategoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 9/26/17.
  */
-@Controller
+//@Controller
+@RestController
 @RequestMapping("/api/v1/categories/")
 public class CategoryController {
 
@@ -23,17 +22,35 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping
+    // used with @Controller annotation
+   /* @GetMapping
     public ResponseEntity<CategoryListDTO> getAllCategories(){
 
         return new ResponseEntity<CategoryListDTO>(
                 new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    }*/
+
+    // used with @RestController annotation
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories(){
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
-    @GetMapping("{name}")
+    // used with @Controller annotation
+   /* @GetMapping("{name}")
     public ResponseEntity<CategoryDTO> getCategoryByName( @PathVariable String name){
         return new ResponseEntity<CategoryDTO>(
                 categoryService.getCategoryByName(name), HttpStatus.OK
         );
+    }*/
+
+    // used with @RestController annotation
+    @GetMapping("{name}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName( @PathVariable String name){
+        return categoryService.getCategoryByName(name);
     }
+
+
 }
